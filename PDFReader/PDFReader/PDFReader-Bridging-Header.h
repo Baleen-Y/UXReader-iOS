e9809cb729b5ee784289142ab8555cbcefed2f1a
yangjx <yangjx@textbooking.com.cn>
2018-12-11 17:36:19 +0800

    修改：库移除

---
+++ PDFReader/PDFReader/PDFReader-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  PDFReader-Bridging-Header.h
+//  PDFReader
+//
+//  Created by xhgc01 on 2018/12/10.
+//  Copyright © 2018 baleen. All rights reserved.
+//
+
+#ifndef PDFReader_Bridging_Header_h
+#define PDFReader_Bridging_Header_h
+// PDF
+#import "UXReader.h"
+
+#endif /* PDFReader_Bridging_Header_h */
---
+++ PDFReader/PDFReader/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  PDFReader
+//
+//  Created by xhgc01 on 2018/12/10.
+//  Copyright © 2018 baleen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UXReaderViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let fileStr = Bundle.main.path(forResource: "foxitpdf", ofType: "pdf")!
+        guard let document = UXReaderDocument.init(url: URL.init(fileURLWithPath: fileStr)) else { return }
+        document.setUseNativeRendering()
+        document.setHighlightLinks(false)
+        document.setShowRTL(false)
+
+        let readerViewController = UXReaderViewController.init()
+        readerViewController.setDocument(document)
+        readerViewController.delegate = self
+        self.present(readerViewController, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func dismiss(_ viewController: UXReaderViewController) {
+        self.presentedViewController?.dismiss(animated: true, completion: nil)
+    }
+}
+
